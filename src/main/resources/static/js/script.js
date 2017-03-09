cb4c4138b15dc89809e58e1d957d314cdf11a648
Matthew <metju312@gmail.com>
2017-03-09 01:55:53 +0100

    added web components: css, js and partial views

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,51 @@
+var app = angular
+        .module("myModule",["ngRoute"])
+        .config(['$locationProvider', function($locationProvider) {
+            $locationProvider.hashPrefix('');
+        }])
+        .config(["$routeProvider", function($routeProvider){
+            $routeProvider
+                .when("/home", {
+                    templateUrl: "partial/home.html",
+                    controller: "homeController"
+                })
+                .when("/search", {
+                    templateUrl: "partial/search.html",
+                    controller: "homeController"
+                })
+                .when("/songs", {
+                    templateUrl: "partial/songs.html",
+                    controller: "songsController"
+                })
+                .when("/playlists", {
+                    templateUrl: "partial/playlists.html",
+                    controller: "songsController"
+                })
+                .when("/login", {
+                    templateUrl: "partial/login.html",
+                    controller: "songsController"
+                })
+                // .otherwise({
+                //     redirectTo: "/index"
+                // })
+        }])
+        .controller("homeController", function ($scope, $http, $log, $location, $anchorScroll){
+
+        })
+        .controller("songsController", function ($scope, $http, $log, $location, $anchorScroll){
+
+            $http({
+                method:'GET',
+                url:"/songs"})
+                    .then(function (response) {
+                        $scope.songs = response.data._embedded.songs;
+                    }, function (reason) {
+                        $scope.error = reason;
+                    });
+
+            $scope.scrollTo = function(scrollLocation){
+                $location.hash(scrollLocation);
+                $anchorScroll.yOffset = 20;
+                $anchorScroll();
+            }
+        });
